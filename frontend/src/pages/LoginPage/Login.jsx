54edf891c3a70b16b004157d1fe7a19ed8188ee2
mi2o2im8 <mi2o2im8@gmail.com>
2026-09-11 18:05:30 +0900

    로그인화면 수정

--- frontend/src/pages/LoginPage/Login.jsx
+++ frontend/src/pages/LoginPage/Login.jsx
@@ -1,8 +1,14 @@
 import "./Login.css";
 import logo from "../../assets/img/logo_name.png";
 
+// 로그인 화면 백엔드 전달 사항 / 기능명: 로그인 / 
+// 항목: 아이디(이메일), 데이터명:email, 입력형식: 이메일
+// 항목: 비밀번호, 데이터명: password, 입력 형식: 비밀번호
+// 로그인 API 필요
+
 function Login() {
   return (
+    // 타이틀 부분
     <div className="Login-container">
       <img src={logo} alt="로고" className="logo" />
 
@@ -10,18 +16,22 @@ function Login() {
         운동으로 연결되는 가장 쉬운 방법!
       </h4>
 
+      {/* 로그인 입력 */}
+      <p className="email-lavel">아이디(이메일)</p>
       <input
         type="text"
-        placeholder="아이디"
+        placeholder="이메일을 입력하세요"
         className="login-input"
       />
-
+      
+      <p className="email-lavel">비밀번호</p>
       <input
         type="password"
-        placeholder="비밀번호"
+        placeholder="비밀번호를 입력하세요"
         className="pw-input"
       />
 
+      {/* 로그인 버튼 */}
       <button className="login-btn">
         로그인
       </button>
---
+++ frontend/src/pages/Signup/SignupProfile.jsx
@@ -0,0 +1,24 @@
+// 2. 기본정보 입력 페이지
+
+import "../Sinup.css";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------
+export default Sinup
