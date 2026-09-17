cb2f54c6b024e8e92cd174b43df82a02dd6da2ec
axvax56 <axvax56@gmail.com>
2026-09-17 18:40:29 +0900

    백엔드 CORS 설정 추가

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:5175",
         "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
